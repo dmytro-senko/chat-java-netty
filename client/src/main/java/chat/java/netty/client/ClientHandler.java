b1a07868405cd966ce2238b37632a90118219edf
Dima Senko <d.senko@i.ua>
2023-05-31 16:37:35 +0300

    <second commit>

---
+++ client/src/main/java/chat/java/netty/client/ClientHandler.java
@@ -0,0 +1,17 @@
+package chat.java.netty.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class ClientHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String message) throws Exception {
+        System.out.println(message);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
